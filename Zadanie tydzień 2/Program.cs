a5f6a229ea416bec0c649e0b18b16be13e329d38
lonys <lonys@ONYSZEK>
2023-11-01 17:57:38 +0100

    Add project files.

---
+++ Zadanie tydzień 2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zadanie: dwie zmienne liczbowe, jedna tekstowa");
+
+int a = 10;
+string b = "Szkoła DotNeta";
+double c = 12.5; 
+
+Console.WriteLine($"Zmienna liczbowa: {a}");
+Console.WriteLine($"Zmienna tekstowa: {b}");
+Console.WriteLine($"Zmienna liczbowa: {c}");
